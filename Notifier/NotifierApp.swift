99d208ca1709dc7c718c74317b5c299bfeda8580
Raidan <92235772+Raidansz@users.noreply.github.com>
2025-02-08 17:23:29 +0100

    Initial Commit

---
+++ Notifier/NotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifierApp.swift
+//  Notifier
+//
+//  Created by Raidan on 2025. 02. 08..
+//
+
+import SwiftUI
+
+@main
+struct NotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
